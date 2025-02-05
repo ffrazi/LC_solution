da1fcf6bae2024ccb7059da104fdbb9918458507
ffrazi <165146728+ffrazi@users.noreply.github.com>
2025-02-05 11:29:19 +0530

    Time: 3 ms (83.21%), Space: 17.6 MB (80.98%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,43 +1,32 @@
 from collections import deque
-
-class Orange:
-    def __init__(self, row, col, days):
-        self.row = row
-        self.col = col
-        self.days = days
+from typing import List
 
 class Solution:
-    def orangesRotting(self, grid):
-        R, C = len(grid), len(grid[0])
-        visited = [[False] * C for _ in range(R)]
-        queue = deque()
-        days, oranges = 0, 0
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        rotten = deque()
+        fresh = 0
+        rows = len(grid)
+        columns = len(grid[0])
 
-        for row in range(R):
-            for col in range(C):
-                if grid[row][col] != 0:
-                    if grid[row][col] == 2:
-                        queue.append(Orange(row, col, 0))
-                        visited[row][col] = True
-                    oranges += 1
+        for i in range(rows):
+            for j in range(columns):
+                if grid[i][j] == 2:
+                    rotten.append((i, j, 0))
+                elif grid[i][j] == 1:
+                    fresh += 1
 
-        if oranges == 0:
-            return 0  
+        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        day = 0
 
-        directions = [(0, -1), (0, 1), (-1, 0), (1, 0)]
-
-        while queue:
-            rotten = queue.popleft()
-            oranges -= 1
-            days = max(days, rotten.days)
-
+        while rotten:
+            r, c, time = rotten.popleft()
             for dr, dc in directions:
-                adjR, adjC = rotten.row + dr, rotten.col + dc
+                newrow = r + dr
+                newcolumn = c + dc
+                if 0 <= newrow < rows and 0 <= newcolumn < columns and grid[newrow][newcolumn] == 1:
+                    grid[newrow][newcolumn] = 2
+                    fresh -= 1
+                    rotten.append((newrow, newcolumn, time + 1))
+                    day = max(day, time + 1)
 
-                if 0 <= adjR < R and 0 <= adjC < C:
-                    if grid[adjR][adjC] == 1 and not visited[adjR][adjC]:
-                        queue.append(Orange(adjR, adjC, rotten.days + 1))
-                        visited[adjR][adjC] = True
-                        grid[adjR][adjC] = 2  
-
-        return -1 if oranges > 0 else days
+        return -1 if fresh > 0 else day
