f5f46fd4d5a09250fede7cd29d3e07260ee0c9ab
ffrazi <165146728+ffrazi@users.noreply.github.com>
2025-02-05 11:26:48 +0530

    Time: 2 ms (71.07%), Space: 42.7 MB (38.62%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,17 +1,26 @@
 import java.util.*;
 
+class Orange {
+    int row, col, days;
+    public Orange(int r, int c, int d) {
+        this.row = r;
+        this.col = c;
+        this.days = d;
+    }
+}
+
 class Solution {
     public int orangesRotting(int[][] grid) {
         int R = grid.length, C = grid[0].length;
         boolean[][] visited = new boolean[R][C];
-        Queue<int[]> queue = new LinkedList<>();
-        int days = -1, oranges = 0;
+        Queue<Orange> queue = new LinkedList<>();
+        int days = 0, oranges = 0;
 
         for (int row = 0; row < R; row++) {
             for (int col = 0; col < C; col++) {
                 if (grid[row][col] != 0) {
                     if (grid[row][col] == 2) {
-                        queue.add(new int[]{row, col});
+                        queue.add(new Orange(row, col, 0));
                         visited[row][col] = true;
                     }
                     oranges++;
@@ -24,22 +33,19 @@ class Solution {
         int[][] directions = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
 
         while (!queue.isEmpty()) {
-            int qsize = queue.size();
-            days++;
+            Orange rotten = queue.poll();
+            oranges--;
+            days = Math.max(days, rotten.days);
 
-            for (int i = 0; i < qsize; i++) {
-                int[] cell = queue.poll();
-                oranges--;
+            for (int index = 0; index < 4; index++) {
+                int adjR = rotten.row + directions[index][0];
+                int adjC = rotten.col + directions[index][1];
 
-                for (int[] dir : directions) {
-                    int adjR = cell[0] + dir[0];
-                    int adjC = cell[1] + dir[1];
-
-                    if (adjR >= 0 && adjR < R && adjC >= 0 && adjC < C 
-                        && grid[adjR][adjC] == 1 && !visited[adjR][adjC]) {
-                        grid[adjR][adjC] = 2;
+                if (adjR >= 0 && adjR < R && adjC >= 0 && adjC < C) {
+                    if (grid[adjR][adjC] == 1 && !visited[adjR][adjC]) {
+                        queue.add(new Orange(adjR, adjC, rotten.days + 1));
                         visited[adjR][adjC] = true;
-                        queue.add(new int[]{adjR, adjC});
+                        grid[adjR][adjC] = 2;  
                     }
                 }
             }
