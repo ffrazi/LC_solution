b1593d26e59d49767be5cc03fb3ceb6057d5566b
ffrazi <165146728+ffrazi@users.noreply.github.com>
2025-09-05 00:19:52 +0530

    Time: 0 ms (100%), Space: 18 MB (12.36%) - LeetHub

---
+++ 3830-find-closest-person/3830-find-closest-person.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findClosest(self, x: int, y: int, z: int) -> int:
+        diff1 = abs(x - z)
+        diff2 = abs(y - z)
+
+        if diff1 < diff2:
+            return 1
+        elif diff2 < diff1:
+            return 2
+        else:
+            return 0
