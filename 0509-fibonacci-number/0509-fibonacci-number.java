b36fcda80b20832c5418810205945815dd6f8a67
ffrazi <165146728+ffrazi@users.noreply.github.com>
2025-12-09 09:16:09 +0530

    Time: 0 ms (100%), Space: 41.9 MB (38.91%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,12 +1,16 @@
 class Solution {
-    //memonization
+    //tabulation
     public int fib(int n) {
-        int[] dp = new int[n+1];
         if (n <= 1) return n;
 
-        if (dp[n] != 0) return dp[n];
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
 
-        dp[n] = fib(n - 1) + fib(n - 2);
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
         return dp[n];
     }
 }
