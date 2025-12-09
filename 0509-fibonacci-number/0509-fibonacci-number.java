3b7a5b079cfa9a8a4b298da60e3e3a470db27009
ffrazi <165146728+ffrazi@users.noreply.github.com>
2025-12-09 09:15:26 +0530

    Time: 32 ms (6.39%), Space: 45.7 MB (5.63%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    //memonization
+    public int fib(int n) {
+        int[] dp = new int[n+1];
+        if (n <= 1) return n;
+
+        if (dp[n] != 0) return dp[n];
+
+        dp[n] = fib(n - 1) + fib(n - 2);
+        return dp[n];
+    }
+}
