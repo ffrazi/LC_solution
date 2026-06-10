9b98eeaae89d37c1fdf535070219fe861cccfa85
ffrazi <165146728+ffrazi@users.noreply.github.com>
2026-06-10 11:08:27 +0530

    Time: 8 ms (11.11%), Space: 51.3 MB (89.73%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,24 +1,39 @@
 class Solution {
-    void change(char a[][],int i,int j){
-        if(i<0 || j<0 ||i==a.length||j==a[0].length||a[i][j]!='1'){
-            return;
-        }
-        a[i][j]=5;
-        change(a,i-1,j);
-        change(a,i+1,j);
-        change(a,i,j-1);
-        change(a,i,j+1);
-    }
     public int numIslands(char[][] grid) {
-        int c=0;
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
+        int m=grid.length;
+        int n=grid[0].length;
+        int cnt=0;
+
+        int[][] dir={{-1,0},{1,0},{0,-1},{0,1}};
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(grid[i][j]=='1'){
-                    c++;
-                    change(grid,i,j);
+                    cnt++;
+
+                    Queue<int[]> q=new LinkedList<>();
+                    q.offer(new int[]{i,j});
+                    grid[i][j]='0';
+
+                    while(!q.isEmpty()){
+                        int[] cur=q.poll();
+                        int x=cur[0];
+                        int y=cur[1];
+
+                        for(int[] d:dir){
+                            int nx=x+d[0];
+                            int ny=y+d[1];
+
+                            if(nx>=0 && nx<m && ny>=0 && ny<n && grid[nx][ny]=='1'){
+                                q.offer(new int[]{nx,ny});
+                                grid[nx][ny]='0';
+                            }
+                        }
+                    }
                 }
             }
         }
-        return c;
+
+        return cnt;
     }
 }
